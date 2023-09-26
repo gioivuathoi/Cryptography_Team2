18c230ea4af91cdfa1998cec6333938c171cf36e
vũ xuân thịnh <caocaodua1@gmail.com>
2023-09-26 23:28:56 +0700

    fisrt update

---
+++ classical/ceasar.py
@@ -0,0 +1,67 @@
+
+# NOTE: NOT DESIGN FOR VIETNAMESE YET. This code work with alphabet and special character (not alphabet) 
+# from 32 to 126 in ascii code. Alphabet in plaintext wil be encrypted by alphabet, special character 
+# wil be encrypted by both alphabet and special characters.
+
+
+def encryption(plain_text, key):
+    cipher_text = ""
+    for pc in plain_text:
+        # Check if the character is lower case
+        if pc.islower():
+            cc = (ord(pc) - 97)%26 + key
+            if cc > 25:
+                cc = cc - 25
+                cipher_text += chr(96 + cc)
+            else:
+                cipher_text += chr(97 + cc)
+        # check if the character is upper case
+        elif pc.isupper():
+            cc = (ord(pc) - 65)%26 + key
+            if cc > 25:
+                cc = cc - 25
+                cipher_text += chr(64+cc)
+            else:
+                cipher_text += chr(65+cc)
+        # check if character is not alphabet
+        else:
+            cc = (ord(pc) - 32)%95 + key
+            if cc > 94:
+                cc = cc - 94
+                cipher_text += chr(31 + cc)
+            else:
+                cipher_text += chr(32 + cc)
+
+    return {"plain_text": plain_text, "key": key, "cipher_text": cipher_text}
+
+
+def decryption(key, cipher_text):
+    plain_text = ""
+    for cc in cipher_text:
+        if cc.islower():
+            dc = (ord(cc)-97)%26-key
+            if dc < 0:
+                plain_text += chr(123 + dc)
+            else:
+                plain_text += chr(97 + dc)
+        elif cc.isupper():
+            dc = (ord(cc)-65)%26-key
+            if dc < 0:
+                plain_text += chr(91+dc)
+            else:
+                plain_text += chr(65+dc)
+        else:
+            dc = (ord(cc) - 32)%95 - key
+            if dc < 0:
+                plain_text += chr(127 + dc)
+            else:
+                plain_text += chr(32 + dc)
+    return plain_text
+
+# Let's test our functions
+plain_text = "I love you, Babe!!!"
+enc = encryption(plain_text, 4)
+print(enc)
+key = enc["key"]
+cipher_text = enc["cipher_text"]
+print(decryption(key, cipher_text))
---
+++ classical/one_time_pad.py
@@ -0,0 +1,89 @@
+import string
+import random
+# NOTE: This code work with alphabet and special character from 32 to 126 in ascii code.
+# Alphabet in plaintext wil be encrypted by alphabet, special character wil be encrypted by both alphabet
+# and special character. Length of keyword equals to plaintext.
+def encryption(plain_text):
+
+    cipher_text = ""
+    # first, let's create a randome keyword which have the same length with plain text
+    length = len(plain_text)
+    randomLetters = random.choices(population=string.ascii_letters, k=length)
+    # now, we will extract the position of those characters in the keyword
+    ids = []
+    for letter in randomLetters:
+        if letter.islower():
+            id = ord(letter) - 97
+        elif letter.isupper():
+            id = ord(letter) - 65
+        ids.append(id)
+
+    # Next, we will create the cipher text based on the keyword
+    for id, character in enumerate(plain_text):
+        # Check if the character is lower type
+        if character.islower():
+            key = (ord(character) - 97)%26 + ids[id]
+            if key > 25:
+                key = key - 25
+                cipher_text += chr(96 + key)
+            else:
+                cipher_text += chr(97 + key)
+        # check if the character is upper type
+        elif character.isupper():
+            key = (ord(character) - 65)%26 + ids[id]
+            if key > 25:
+                key = key-25
+                cipher_text += chr(64+key)
+            else:
+                cipher_text += chr(65+key)
+        # check if the character is special character (not alphabet)
+        else:
+            key = (ord(character) - 32)%95 + ids[id]
+            if key > 94:
+                key = key - 94
+                cipher_text += chr(31 + key)
+            else:
+                cipher_text += chr(32 + key)
+    return {"plain_text": plain_text, "keyword": randomLetters, "cipher_text": cipher_text}
+    # print(cipher_text)
+        
+
+def decryption(keyword, cipher_text):
+    #From the keyword, we will extract the shift number, which is the position of character in alphabet
+    ids = []
+    plain_text = ""
+    for letter in keyword:
+        if letter.islower():
+            id = ord(letter) - 97
+        elif letter.isupper():
+            id = ord(letter) - 65
+        ids.append(id)
+    for id, character in enumerate(cipher_text):
+        if character.islower():
+            key = (ord(character) - 97)%26 - ids[id]
+            if key < 0:
+                plain_text += chr(123 + key)
+            else:
+                plain_text += chr(97 + key)
+        elif character.isupper():
+            key = (ord(character) - 65)%26 - ids[id]
+            if key < 0:
+                plain_text += chr(91+key)
+            else:
+                plain_text += chr(65+key)
+        else:
+            key = (ord(character) - 32)%95 - ids[id]
+            if key < 0:
+                plain_text += chr(127 + key)
+            else:
+                plain_text += chr(32 + key)
+    return plain_text
+
+
+# Let's test our functions
+plain_text = "I love you, Babe!!!!"
+enc = encryption(plain_text)
+print(enc)
+keyword = enc["keyword"]
+cipher_text = enc["cipher_text"]
+print(decryption(keyword, cipher_text))
